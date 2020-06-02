afe7005723b4c5b2e8cab4564bcf6bd2cdfea5e9
Adam Vecsi <adamvecsi94@gmail.com>
2020-06-02 20:40:58 +0200

    messages fix

--- server.js
+++ server.js
@@ -1,108 +1,61 @@
 // Dependencies.
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var socketIO = require("socket.io");
+var uuid = require("uuid-random");
 
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
+
 var port = 8080;
 
-var ball = {
-  x: 780,
-  y: 200
-};
-var players = {};
-var counter = 1;
-var redGoals = 0;
-var blueGoals = 0;
-var playersIds =[];
-
-app.set('port', port);
-app.use('/static', express.static(__dirname + '/static'));
+app.set("port", port);
+app.use("/static", express.static(__dirname + "/static"));
 
 // Routing
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(port, function() {
-  console.log('Starting server on port ' + port);
+server.listen(port, function () {
+  console.log("Starting server on port " + port);
 });
 
-io.on('connection', function(socket) {
-  var playersSize = Object.keys(players).length + 1;
-  if(playersSize <= 2 ){
-    if(playersSize === 1){
-      newPlayer(socket, 20, 175);
-      
-    } else {
-      newPlayer(socket, 740, 175);
-    }
-  } else {
-      console.log("Players limit max 2");
-  }
-  movement(socket);
-});
-
-function newPlayer(socket, x, y) {
-  socket.on('new player', function() {
+var players = {};
+io.on("connection", function (socket) {
+  // reciving new message
+  socket.on("new player", function () {
     players[socket.id] = {
-      id: socket.id,
-      x: x,
-      y: y,
-      color: counter
+      x: 300,
+      y: 300,
     };
-    console.log(players)
-    console.log("Number of players: " + counter)
-    counter++;
-    playersIds.push(socket.id);
   });
-}
-
-function movement(socket) {
-  socket.on('movement', function(playerMove, ballMove) {
+  // reciving new message
+  socket.on("movement", function (data) {
     var player = players[socket.id] || {};
-
-    if (player.id === playersIds[0] && playerMove.redUp && player.y >= 0 ) {
-      player.y -= 1;
+    console.log(players);
+    if (data.left) {
+      player.x -= 5;
     }
-    if (player.id === playersIds[0] && playerMove.redDown && player.y <= 360) {
-      player.y += 1;
+    if (data.up) {
+      player.y -= 5;
     }
-    if (player.id === playersIds[1] && playerMove.blueUp && player.y >= 0 ) {
-      player.y -= 1;
+    if (data.right) {
+      player.x += 5;
     }
-    if (player.id === playersIds[1] && playerMove.blueDown && player.y <= 360) {
-      player.y += 1;
+    if (data.down) {
+      player.y += 5;
     }
-
-    if(ballMove.up && ball.y > 0){
-      ball.y -= 1;
-    }
-    if(ballMove.down && ball.y < 400){
-      ball.y += 1;
-    }
-
-   if(ballMove.right && ball.x < 780){
-      ball.x += 5;
-      if(ball.x === 780 && ball.y >= 100 && ball.y <= 300){
-        redGoals++;
-        console.log('Result: blue: ' + blueGoals + ' ,red: ' + redGoals);
-      }
-    }
-   
-    if(ballMove.left && ball.x > 20){
-      ball.x -= 5;
-      if(ball.x === 20 && ball.y >= 100 && ball.y <= 300 ){
-        blueGoals++;
-        console.log('Result: blue: ' + blueGoals + ' ,red: ' + redGoals);
-      }
-    } 
   });
-}
+});
 
-setInterval(function() {
-  io.sockets.emit('state', players, ball, playersIds, blueGoals, redGoals);
+setInterval(function () {
+  var message = new Object();
+  message.messageId = uuid();
+  message.players  = players;
+  //sending message
+  io.sockets.emit("state", message);
+  //console.log(message);
 }, 1000 / 60);
--- static/client.js
+++ static/client.js
@@ -1,153 +1,64 @@
 var socket = io();
 
-var playerMovement = {
-  redUp: true,
-  redDown: false,
-  blueUp: true,
-  blueDown: false
-}
-
-var ballMovement = {
+var movement = {
+  up: false,
+  down: false,
   left: false,
   right: false,
-  up: false,
-  down: false
-}
-
-var redGoalKeeper = false;
-var blueGoalKeeper = false;
-/*document.addEventListener('keydown', function(event) {
+};
+document.addEventListener("keydown", function (event) {
   switch (event.keyCode) {
-    case 37: // left arrow
-      ballMovement.left = true;
-      redGoalKeeper = false;
-      blueGoalKeeper = false;
+    case 65: // A
+      movement.left = true;
       break;
-    case 38: // up arrow
-      ballMovement.up = true;
-      redGoalKeeper = false;
-      blueGoalKeeper = false;
+    case 87: // W
+      movement.up = true;
       break;
-    case 39: // right arrow
-      ballMovement.right = true;
-      redGoalKeeper = false;
-      blueGoalKeeper = false;
+    case 68: // D
+      movement.right = true;
       break;
-    case 40: // down arrow
-      ballMovement.down = true;
-      redGoalKeeper = false;
-      blueGoalKeeper = false;
+    case 83: // S
+      movement.down = true;
       break;
   }
 });
-document.addEventListener('keyup', function(event) {
+document.addEventListener("keyup", function (event) {
   switch (event.keyCode) {
-    case 37: // left arrow
-      ballMovement.left = false;
+    case 65: // A
+      movement.left = false;
       break;
-    case 38: // up arrow
-      ballMovement.up = false;
+    case 87: // W
+      movement.up = false;
       break;
-    case 39: // right arrow
-      ballMovement.right = false;
+    case 68: // D
+      movement.right = false;
       break;
-    case 40: // down arrow
-      ballMovement.down = false;
+    case 83: // S
+      movement.down = false;
       break;
   }
-});*/
+});
 
-
-socket.emit('new player');
-setInterval(function() {
-  socket.emit('movement', playerMovement, ballMovement);
+//sending message
+socket.emit("new player");
+setInterval(function () {
+  //sending message
+  socket.emit("movement", movement);
 }, 1000 / 60);
 
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById("canvas");
 canvas.width = 800;
-canvas.height = 400;
-var context = canvas.getContext('2d');
-socket.on('state', function(players, ball, playersIds, blueGoals, redGoals, redGoalKeeper, blueGoalKeeper) {
+canvas.height = 600;
+var context = canvas.getContext("2d");
+//reciveing message
+socket.on("state", function (message) {
+  console.log(message);
   context.clearRect(0, 0, 800, 600);
-  if(ball.x === 780 && ball.y >= 100 && ball.y <= 300){
-    console.log('Result: blue: ' + blueGoals + ' ,red: ' + redGoals);
-    blueGoalKeeper = true;
-    redGoalKeeper = false;
-    ballMovement.right = false;
-  }
-  if(ball.x === 20 && ball.y >= 100 && ball.y <= 300){
-    console.log('Result: blue: ' + blueGoals + ' ,red: ' + redGoals);
-    redGoalKeeper = true;
-    blueGoalKeeper = false;
-    ballMovement.left = false;
-  }
-  
-  for (var id in players) {
-    var player = players[id];
-    if(player.y === 100 || player.y < 100){
-      if(player.id === playersIds[0]){
-        playerMovement.redUp = false;
-        playerMovement.redDown = true;
-      }
-      if(player.id === playersIds[1]){
-        playerMovement.blueUp = false;
-        playerMovement.blueDown = true;
-      }
-    }
-    if(player.y === 260 || player.y > 260){
-      if(player.id === playersIds[0]){
-        playerMovement.redUp = true;
-        playerMovement.redDown = false;
-      }
-      if(player.id === playersIds[1]){
-        playerMovement.blueUp = true;
-        playerMovement.blueDown = false;
-      }
-    }
-
-    if(redGoalKeeper && player === players[playersIds[0]]){
-      ball.x = player.x + 60;
-      ball.y = player.y + 20;
-      setTimeout(function(){ 
-        ballMovement.right = true;
-        redGoalKeeper = false;
-      }, 3000);
-    }
-
-    if(blueGoalKeeper && player === players[playersIds[1]]){
-      ball.x = player.x - 20;
-      ball.y = player.y + 20;
-      setTimeout(function(){
-        ballMovement.left = true;
-        blueGoalKeeper = false;
-      }, 3000);
-    }
-
-    if(player === players[playersIds[0]]){
-      //red players
-      context.fillStyle = 'red';
-      context.beginPath();
-      context.fillRect(player.x, player.y, 40, 40);
-      //context.fillRect(200, 50, 40, 40);
-      //context.fillRect(200, 300, 40, 40);
-      //gate
-      context.fillStyle = 'white';
-      context.fillRect(0, 100, 10, 200);
-    }
-    if(player === players[playersIds[1]]){
-      //blue players
-      context.fillStyle = 'blue';
-      context.beginPath();
-      context.fillRect(player.x, player.y, 40, 40);
-      //context.fillRect(600, 50, 40, 40);
-      //context.fillRect(600, 300, 40, 40);
-      //gate
-      context.fillStyle = 'white';
-      context.fillRect(790, 100, 10, 200);
-      //ball
-      context.fillStyle = 'white';
-      context.arc(ball.x, ball.y, 20, 0, 2 * Math.PI);
-      context.fill();
-    }
+  context.fillStyle = "blue";
+  for (var id in message.players) {
+    var player = message.players[id];
+    context.beginPath();
+    context.fillRect(player.x, player.y, 40, 60);
+    context.fill();
   }
 });
